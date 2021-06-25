9da8ee26f6258f8a8060abc3cc0d6404146068da
zhouyuan <circle.z.yuan@gmail.com>
2021-06-25 23:42:16 +0800

    实作 router handler

--- src/HttpRouterHandler.cpp
+++ src/HttpRouterHandler.cpp
@@ -10,7 +10,28 @@
 
 #include "HttpRouterHandler.h"
 
+#include "ViewRequestHandler.h"
+#include "BuyRequestHandler.h"
+
 HttpRouterHandler* HttpRouterHandler::GetInstance() {
   static HttpRouterHandler instance;
   return &instance;
+}
+
+void HttpRouterHandler::RouteHttpRequest(
+    const HttpRequestPacket& http_request_packet,
+    HttpResponsePacket* http_response_packet) {
+  RequestHandler* request_handler;
+
+  if (http_request_packet.path.compare("/view") == 1) {
+    request_handler = new ViewRequestHandler();
+  } else if (http_request_packet.path.compare("/buy") == 1) {
+    request_handler = new BuyRequestHandler();
+  } else {
+    http_response_packet->status_code = HttpResponsePacket::not_found;
+    http_response_packet->content = "{\"msg\":\"Resource not found\"}";
+  }
+
+  request_handler->ProcessBusinessLogic(http_request_packet,
+                                        http_response_packet);
 }
